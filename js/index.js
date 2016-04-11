41905c29db5d94eec86c91b69c9f5210223416a0
CBenni <levelcarnage@gmail.com>
2016-04-11 16:56:14 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+var config = {
+	content: [{
+		type: 'row',
+		content: [
+			{
+				type:'component',
+				componentName: 'stream',
+				title: "stream: cbenni",
+				componentState: { channel: 'cbenni' }
+			},
+			{
+				type:'component',
+				componentName: 'chat',
+				title: "chat: cbenni",
+				componentState: { channel: 'cbenni' }
+			}
+		]
+	}]
+};
+
+var myLayout = new GoldenLayout( config );
+
+myLayout.registerComponent( 'stream', function( container, state ){
+  container.getElement().append($('<iframe src="https://player.twitch.tv/?channel='+state.channel+'" frameborder="0" scrolling="no"></iframe>'));
+});
+myLayout.registerComponent( 'chat', function( container, state ){
+  container.getElement().append($('<iframe src="https://www.twitch.tv/'+state.channel+'/chat?popout=" frameborder="0" scrolling="no"></iframe>'));
+});
+
+myLayout.init();
+
+function addTab(newTabConfig) {
+	myLayout.root.contentItems[0].addChild(newTabConfig)
+}
+
+$(document).contextmenu({
+		delegate: "*",
+		autoFocus: true,
+		preventContextMenuForPopup: true,
+		preventSelect: true,
+		taphold: true,
+		menu: [
+			{title: "Add stream...", cmd: "s"},
+			{title: "Add chat...", cmd: "c"},
+			{title: "Add stream+chat...", cmd: "s+c"}
+		],
+		// Handle menu selection to implement a fake-clipboard
+		select: function(event, ui) {
+			var channel = prompt("Channel to add");
+			if(channel === undefined || channel === "") {
+				return
+			}
+			var $target = ui.target;
+			for(var i=0;i<ui.cmd.length;++i) {
+				var cmd = ui.cmd[i];
+				if(cmd=="s") {
+					addTab({
+						type:'component',
+						componentName: 'stream',
+						title: "stream: "+channel,
+						componentState: { channel: channel }
+					});
+				}
+				else if(cmd=="c") {
+					addTab({
+						type:'component',
+						componentName: 'chat',
+						title: "chat: "+channel,
+						componentState: { channel: channel }
+					});
+				}
+			}
+		}
+	});
